7ee31bae4c13b333056f79be6584f7642cc1ed6f
abit <sakiba@myumanitoba.ca>
2019-07-01 17:41:29 -0500

    Initial Commit

---
+++ SplitIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitIt
+//
+//  Created by Abir Ahammed on 2019-07-01.
+//  Copyright © 2019 Abir Ahammed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
